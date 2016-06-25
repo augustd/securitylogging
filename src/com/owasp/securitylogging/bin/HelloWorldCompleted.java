9b4ad8dd802c961f71298f91df1afc986b791483
Milton Smith <milton.smith.rr@gmail.com>
2016-06-24 18:42:50 -0700

    update the final class

--- src/com/owasp/securitylogging/bin/HelloWorldCompleted.java
+++ src/com/owasp/securitylogging/bin/HelloWorldCompleted.java
@@ -1,5 +1,8 @@
 package com.owasp.securitylogging.bin;
 
+import org.owasp.security.logging.util.IntervalLoggerController;
+import org.owasp.security.logging.util.SecurityLoggingFactory;
+import org.owasp.security.logging.util.SecurityUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,17 +10,32 @@ public class HelloWorldCompleted {
 
 	public static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
 	
-	public HelloWorldCompleted() {
+	public HelloWorldCompleted(String[] args) {
 
 		logger.info("It's alive!");
 		
+		// log command line arguments
+		SecurityUtil.logCommandLineArguments(args);
+				
+		// log shell environment variables
+		SecurityUtil.logShellEnvironmentVariables();
+				
+		// log java system properties
+		SecurityUtil.logJavaSystemProperties();
+		
+		// start the interval logger
+		IntervalLoggerController wd = SecurityLoggingFactory.getControllerInstance();
+		wd.start();
+
+		// Intercept system streams.
+		SecurityUtil.bindSystemStreamsToSLF4J();
 	}
 	
 	public static void main(String[] args) {
 	
 		try {
 			
-			new HelloWorld();
+			new HelloWorldCompleted(args);
 			
 		}catch( Throwable t ) {
 			t.printStackTrace();
